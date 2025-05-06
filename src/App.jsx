3e94e48502cf9890d5505627117e57c6dbe2fb31
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2025-05-06 22:42:02 +0530

    feat: add tabbed navigation for patient registration and search
    - Implement tab component with useState for PatientForm/SearchPatients
    - Add centered tab UI with Tailwind CSS and active tab highlighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 import PatientForm from "./components/PatientForm";
+import SearchPatients from "./components/SearchPatients";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("register");
+
   return (
-    <div className="bg-bg">
-      <PatientForm />
+    <div className="bg-bg w-full h-screen flex flex-col items-center ">
+      <div className="w-full mt-8 ">
+        <nav className="flex max-w-xl mx-auto border-b-4 justify-center border-primary ">
+          <ul className="flex flex-row gap-8">
+            <li
+              className={`text-lg cursor-pointer px-4 py-2 ${
+                selectedTab === "register"
+                  ? "text-bg  border-primary bg-primary rounded-t-lg"
+                  : "text-gray-600 hover:text-primary"
+              }`}
+              onClick={() => setSelectedTab("register")}
+            >
+              Patient Registration
+            </li>
+            <li
+              className={`text-lg cursor-pointer px-4 py-2 ${
+                selectedTab === "search"
+                  ? "text-bg  border-primary bg-primary rounded-t-lg"
+                  : "text-gray-600 hover:text-primary"
+              }`}
+              onClick={() => setSelectedTab("search")}
+            >
+              Search Patients using Query
+            </li>
+          </ul>
+        </nav>
+        {selectedTab === "register" && <PatientForm />}
+        {!selectedTab === "search" && <SearchPatients />}
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchPatients.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { PGlite } from "@electric-sql/pglite";
+
+const db = new PGlite("idb://my-pgdata");
+
+const SearchPatients = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    </div>
+  );
+};
+
+export default SearchPatients;
