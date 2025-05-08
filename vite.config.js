a5cc816a9d4224f9f766845661bdafafc3306757
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2025-05-09 01:24:07 +0530

    Fix: vercel deployment fix

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   assetsInclude: ["**/*.wasm"],
   optimizeDeps: {
     exclude: ["@electric-sql/pglite"],
+  },
+  build: {
+    target: "esnext",
   },
 });
